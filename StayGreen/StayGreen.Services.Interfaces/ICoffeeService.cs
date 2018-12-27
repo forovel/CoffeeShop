75bd52d02d953e95179c0abba0722c370f2b98aa
Oleh Yashchyshy <oyashchyshyn@bizico.com>
2018-12-27 20:03:54 +0200

    clean up code

--- StayGreen/StayGreen.Services.Interfaces/ICoffeeService.cs
+++ StayGreen/StayGreen.Services.Interfaces/ICoffeeService.cs
@@ -1,10 +1,11 @@
-﻿using StayGreen.Models.Schema;
+﻿using StayGreen.Models.Dtos.Schemas.Coffee;
+using StayGreen.Models.Schema;
 using StayGreen.Services.Interfaces.Common;
 using System;
 
 namespace StayGreen.Services.Interfaces
 {
-    public interface ICoffeeService : IBaseService<Coffee, Coffee, Coffee, Guid>
+    public interface ICoffeeService : IBaseService<CoffeeReadDto, Coffee, Coffee, Guid>
     {
     }
 }
